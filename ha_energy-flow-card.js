cd7d91bc8e8838c8ea6ec17aa9b117832567d0d3
Marco Meister <55204544+psym88@users.noreply.github.com>
2026-08-05 16:28:47 +0200

    Release v1.2.3

--- ha_energy-flow-card.js
+++ ha_energy-flow-card.js
@@ -1,6 +1,6 @@
 // HA Energy Flow Card
 
-const CARD_VERSION = "1.2.2";
+const CARD_VERSION = "1.2.3";
 const CARD_TAG = "ha_energy-flow-card";
 const EDITOR_TAG = "ha_energy-flow-card-editor";
 const HOLD_DELAY_MS = 500;
